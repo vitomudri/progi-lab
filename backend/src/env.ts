95654b8f224069a96bd65b628cd1cee7077c3a5a
vitomudri <vito.mudri@fer.hr>
2025-11-08 18:59:16 +0100

    Swap PRODUCTION to "standard" NODE_ENV variable

--- backend/src/env.ts
+++ backend/src/env.ts
@@ -1,21 +1,12 @@
 import { z } from "zod";
 import dotenv from "dotenv";
 
-const dev_check_schema = z.object({
-    PRODUCTION: z
-        .string()
-        .default("false")
-        .transform((v) => v !== "false")
-});
+const PRODUCTION: boolean = process.env.NODE_ENV === "production";
 
 const schema = z.object({
     HOST: z.string().default("localhost"),
     PORT: z.coerce.number().int().positive().default(3000),
     CORS_ORIGIN: z.string().default("http://localhost:3000"),
-    PRODUCTION: z
-        .string()
-        .default("false")
-        .transform((v) => v !== "false"),
     PG_HOST: z.string(),
     PG_PORT: z.coerce.number().int().positive().default(5432),
     PG_USER: z.string(),
@@ -23,13 +14,7 @@ const schema = z.object({
     PG_PASS: z.string()
 });
 
-let dev_check = dev_check_schema.safeParse(process.env);
-
-if (!dev_check.success) {
-    throw new Error("This should be unreachable! Error while parsing dev_check_schema");
-}
-
-if (!dev_check.data.PRODUCTION) {
+if (!PRODUCTION) {
     dotenv.config();
 }
 
@@ -39,4 +24,7 @@ if (!parsed.success) {
     throw new Error("Failed to parse environment: invalid or missing variables");
 }
 
-export const env = parsed.data;
+export const env = {
+    PRODUCTION,
+    ...parsed.data
+};
