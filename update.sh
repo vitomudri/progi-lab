f2f5007b82878a338316015881dfd7265e0b8cc3
vitomudri <vito.mudri@fer.hr>
2025-11-09 18:41:57 +0100

    Add notifications to update script

--- update.sh
+++ update.sh
@@ -5,6 +5,25 @@ WORKDIR=$(dirname -- "$(readlink -f -- "$0")")
 pushd "$WORKDIR/docker" &>/dev/null
 
 docker compose pull
+
+if [[ $? -ne 0 ]]; then
+    export DISCORD_TITLE="Update failed!"
+    export DISCORD_DESCRIPTION="The update script has failed on stage: pull image"
+    "$WORKDIR/notification.sh"
+    exit 1
+fi
+
 docker compose up -d
 
+if [[ $? -ne 0 ]]; then
+    export DISCORD_TITLE="Update failed!"
+    export DISCORD_DESCRIPTION="The update script has failed on stage: recreate containers"
+    "$WORKDIR/notification.sh"
+    exit 1
+fi
+
 popd &>/dev/null
+
+export DISCORD_TITLE="Update completed."
+export DISCORD_DESCRIPTION="The update script was run and completed successfully."
+"$WORKDIR/notification.sh"
