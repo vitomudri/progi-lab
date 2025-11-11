29a82a7cdc9449caf4110c17559a836944ac77e0
vitomudri <vito.mudri@fer.hr>
2025-11-12 00:45:06 +0100

    Fix broken usage of `User.ts` after modifications

--- backend/src/models/testUser.ts
+++ backend/src/models/testUser.ts
@@ -1,25 +1,24 @@
-import { User } from './User.js';
+import { User } from "./User.js";
 
 async function test() {
-  console.log('Provjeravam postoji li email...');
-  const exists = await User.existsByEmail('mia@example.com');
-  console.log('Postoji?', exists);
+    console.log("Provjeravam postoji li email...");
+    const exists = await User.exists_in_db({ email: "mia@example.com" });
+    console.log("Postoji?", exists);
 
-  if (!exists) {
-    console.log('➡ Ne postoji. Spremam novog...');
-    const novi = new User({
-      ime: 'Mia',
-      prezime: 'Zoroja',
-      email: 'mia@example.com',
-      lozinka: 'tajna123'
-    });
-    await novi.save();
-    console.log('Novi user spremljen.');
-  }
+    if (!exists) {
+        console.log("➡ Ne postoji. Spremam novog...");
+        const novi = new User({
+            first_name: "Mia",
+            last_name: "Zoroja",
+            email: "mia@example.com"
+        });
+        await novi.save();
+        console.log("Novi user spremljen.");
+    }
 
-  console.log('➡ Dohvaćam usera...');
-  const user = await User.getByEmail('mia@example.com');
-  console.log('User dobiven:', user);
+    console.log("➡ Dohvaćam usera...");
+    const user = await User.get_from_db({ email: "mia@example.com" });
+    console.log("User dobiven:", user);
 }
 
 test();
