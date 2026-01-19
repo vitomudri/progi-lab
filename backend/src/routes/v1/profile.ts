b917329b0090a15d14c088af7f4dc3d6f7daa0d4
vitomudri <vito.mudri@fer.hr>
2026-01-19 20:54:04 +0100

    Add profile API path

--- backend/src/routes/v1/profile.ts
+++ backend/src/routes/v1/profile.ts
@@ -1,5 +1,8 @@
 import express from "express";
 import { require_auth } from "../../middleware/auth.js";
+import { User } from "../../models/User.js";
+import { z } from "zod";
+import type { UUID } from "crypto";
 
 const profile_router = express.Router();
 
@@ -18,13 +21,27 @@ profile_router.get("/me", require_auth, async (req, res) => {
     });
 });
 
-profile_router.get("/:id", async (req, res) => {
-    const requested_id = req.params.id;
-    if (req.user) {
-        const user = req.user!;
+profile_router.get("/:id", require_auth, async (req, res) => {
+    const user = req.user!;
+    const maybe_requested_id = req.params.id;
 
-    } else {
-
+    try {
+        const requested_id: UUID = z.uuidv4().parse(maybe_requested_id) as UUID;
+        const requested_user = await User.from_db({ user_id: requested_id });
+        if (requested_user) {
+            return res.json({
+                user_id: requested_user.user_id,
+                first_name: requested_user.first_name,
+                last_name: requested_user.last_name,
+                registration_date: requested_user.registration_date,
+                status: requested_user.status,
+                role: requested_user.role,
+            });
+        } else {
+            return res.status(400).json({ error: "Invalid user ID" });
+        }
+    } catch(err) {
+        return res.status(400).json({ error: "Invalid user ID" });
     }
 });
 
